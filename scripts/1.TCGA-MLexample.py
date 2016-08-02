ae27311b684c31bacfee7d0dd2be9f418d7301fc
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-08-02 12:57:45 -0500

    Use grid_search in notebook and add visualization (#18)
    Addresses issues with example notebook brought up at July 26 meetup:
    
    1. Standardize training and testing separately
    2. Use AUROC on continuous rather than binary predictions
    
    Clean up variable names. Simplify to to testing/training terminology. No more
    "hold out".
    
    Use sklearn.grid_search.GridSearchCV to optimize hyperparameters. Expand range
    of l1_ratio and alpha. Specify random_state in GridSearchCV, which should
    prevent having to set the seed manually using the random module. Grid search
    should enable a more modular architecture enabling swapping in different
    algorithms as long as their `param_grid` is defined.
    
    Add exploratory analysis of predictions.
    
    Add parallel processing using joblib to speed up cross validation.
    
    Remove median absolute deviation feature selection. This step had to be removed
    or modified because it used testing data for feature selection.

--- scripts/1.TCGA-MLexample.py
+++ scripts/1.TCGA-MLexample.py
@@ -1,247 +1,250 @@
 
 # coding: utf-8
 
+# # Create a logistic regression model to predict TP53 mutation from gene expression data in TCGA
+
 # In[1]:
 
-get_ipython().magic('matplotlib inline')
+import os
+import urllib
+import random
+import warnings
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import preprocessing, grid_search
+from sklearn.linear_model import SGDClassifier
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import roc_auc_score, roc_curve
 
 
 # In[2]:
 
-import os
-import urllib
-import pandas as pd
-import random
-from statsmodels.robust import scale
-from sklearn import preprocessing
-from sklearn.linear_model import SGDClassifier
-from sklearn.cross_validation import StratifiedKFold, train_test_split
-from sklearn.metrics import roc_auc_score, roc_curve
-import matplotlib.pyplot as plt
-import seaborn as sns
+get_ipython().magic('matplotlib inline')
+plt.style.use('seaborn-notebook')
 
+
+# ## Specify model configuration
 
 # In[3]:
 
-random.seed(123)
+# We're going to be building a 'TP53' classifier 
+GENE = 'TP53'
 
 
 # In[4]:
 
-# We're going to be building a 'TP53' logistic regression classifier with
-# an elastic net penalty
-GENE = 'TP53'
-CLASSIFIER = 'log'
-PENALTY = 'elasticnet'
-
-
-# In[5]:
-
 # Parameter Sweep for Hyperparameters
-alpha_params = [0.05, 0.1, 0.2, 0.3]
-l1_params = [0.05, 0.1, 0.2, 0.3]
+param_grid = {
+    'loss': ['log'],
+    'penalty': ['elasticnet'],
+    'alpha': [10 ** x for x in range(-6, 1)],
+    'l1_ratio': [0, 0.05, 0.1, 0.2, 0.5, 0.8, 0.9, 0.95, 1],
+}
 
 
 # *Here is some [documentation](http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.SGDClassifier.html) regarding the classifier and hyperparameters*
 # 
 # *Here is some [information](https://ghr.nlm.nih.gov/gene/TP53) about TP53*
 
-# # Load Data
+# ## Load Data
 
-# In[6]:
+# In[5]:
 
 if not os.path.exists('data'):
     os.makedirs('data')
 
 
+# In[6]:
+
+url_to_path = {
+    # X matrix
+    'https://ndownloader.figshare.com/files/5514386':
+        os.path.join('data', 'expression.tsv.bz2'),
+    # Y Matrix
+    'https://ndownloader.figshare.com/files/5514389':
+        os.path.join('data', 'mutation-matrix.tsv.bz2'),
+}
+
+for url, path in url_to_path.items():
+    if not os.path.exists(path):
+        urllib.request.urlretrieve(url, path)
+
+
 # In[7]:
 
-# Download X Matrix
-url = 'https://ndownloader.figshare.com/files/5514386'
-if not os.path.exists('data/expression.tsv.bz2'):
-    urllib.request.urlretrieve(url, os.path.join('data', 'expression.tsv.bz2'))
+get_ipython().run_cell_magic('time', '', "path = os.path.join('data', 'expression.tsv.bz2')\nX = pd.read_table(path, index_col=0)")
 
 
 # In[8]:
 
-X = pd.read_table(os.path.join('data', 'expression.tsv.bz2'), index_col=0)
+get_ipython().run_cell_magic('time', '', "path = os.path.join('data', 'mutation-matrix.tsv.bz2')\nY = pd.read_table(path, index_col=0)")
 
+
+# ## Process Mutation Matrix
 
 # In[9]:
-
-# Download Y Matrix
-url = "https://ndownloader.figshare.com/files/5514389"
-if not os.path.exists('data/mutation-matrix.tsv.bz2'):
-    urllib.request.urlretrieve(url, os.path.join('data', 'mutation-matrix.tsv.bz2'))
-
-
-# In[10]:
-
-Y = pd.read_table(os.path.join('data', 'mutation-matrix.tsv.bz2'), index_col=0)
-
-
-# # Process Mutation Matrix
-
-# In[11]:
 
 y = Y[GENE]
 
 
-# In[12]:
+# In[10]:
 
 # The Series now holds TP53 Mutation Status for each Sample
 y.head(6)
 
 
-# In[13]:
+# In[11]:
 
 # Here are the percentage of tumors with NF1
 y.value_counts(True)
 
 
-# # Process X Matrix
+# ## Set aside 10% of the data for testing
+
+# In[12]:
+
+# Typically, this can only be done where the number of mutations is large enough
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
+'Size: {:,} features, {:,} training samples, {:,} testing samples'.format(len(X.index), len(X_train), len(X_test))
+
+
+# ## Feature transformation: Standardization
+
+# In[13]:
+
+def scale_df(df):
+    """Standardize colummns by scaling to z-scores"""
+    scaled_matrix = preprocessing.scale(df)
+    return pd.DataFrame(scaled_matrix, index=df.index, columns=df.columns)
+
+# Standardize X separately for the holdout dataset
+X_train, X_test = scale_df(X_train), scale_df(X_test)
+
+
+# ## Cross validation model fitting
 
 # In[14]:
 
-# Subset X matrix to only 5,000 highest MAD (median absolute deviation) genes
-all_mad_genes = scale.mad(X, c=1, axis=0)
-all_mad_genes = pd.Series(all_mad_genes,
-                          index=X.columns.values)
-all_mad_genes = all_mad_genes.sort_values(ascending=False)
+# joblib is used to cross-validate in parallel by setting `n_jobs=-1` in GridSearchCV
+# Supress joblib warning. See https://github.com/scikit-learn/scikit-learn/issues/6370
+warnings.filterwarnings('ignore', message='Changing the shape of non-C contiguous array')
 
 
 # In[15]:
 
-X_sub = X[all_mad_genes.index[:5000]]
+get_ipython().run_cell_magic('time', '', "clf = SGDClassifier(random_state=0, class_weight='balanced')\nclf_grid = grid_search.GridSearchCV(estimator=clf, param_grid=param_grid, n_jobs=-1, scoring='roc_auc')\n# Fit the model (the computationally intensive part)\nclf_grid.fit(X=X_train, y=y_train)\nbest_clf = clf_grid.best_estimator_")
 
 
 # In[16]:
 
-# Normalize X matrix by Z-scoring genes
-X_matrix_scale = preprocessing.scale(X_sub)
-X_matrix_sub = pd.DataFrame(X_matrix_scale, index=X_sub.index, columns=X_sub.columns)
+clf_grid.best_params_
 
-
-# # Hold Out 10% of the data
 
 # In[17]:
 
-# Typically, this can only be done where the number of mutations is large enough
-X_matrix, X_holdout, Y_matrix, Y_holdout = train_test_split(
-    X_matrix_sub, y, test_size=0.1, random_state=123)
+best_clf
 
 
-# # Cross Validation to select Hyperparameters
+# ## Visualize hyperparameters performance
 
 # In[18]:
 
-# 5 fold cross validation ensuring balanced status membership
-cv_accuracy = []
-skf = StratifiedKFold(Y_matrix, 5, random_state=123)
-for train_idx, test_idx in skf:
-    X_train, X_test = X_matrix.ix[train_idx], X_matrix.ix[test_idx]
-    Y_train, Y_test = Y_matrix.ix[train_idx], Y_matrix.ix[test_idx]
-    for alpha_param in alpha_params:
-        for l1_param in l1_params:
-            
-            # Initialize logistic regression classifier
-            clf = SGDClassifier(loss=CLASSIFIER,
-                    penalty=PENALTY,
-                    alpha=alpha_param,
-                    l1_ratio=l1_param,
-                    class_weight='balanced',
-                    random_state=123)
-            
-            # Fit the model
-            clf.fit(X_train, Y_train)
-            predict_y_train = clf.predict(X_train)
-            predict_y_test = clf.predict(X_test)
-            
-            # Get the train/test AUROC balanced by class
-            train_error = roc_auc_score(Y_train, predict_y_train, average='weighted')
-            test_error = roc_auc_score(Y_test, predict_y_test, average='weighted')
-            
-            # Obtain metrics for plotting
-            cv_accuracy.append(['train', train_error, alpha_param, l1_param])
-            cv_accuracy.append(['test', test_error, alpha_param, l1_param])
+def grid_scores_to_df(grid_scores):
+    """
+    Convert a sklearn.grid_search.GridSearchCV.grid_scores_ attribute to 
+    a tidy pandas DataFrame where each row is a hyperparameter-fold combinatination.
+    """
+    rows = list()
+    for grid_score in grid_scores:
+        for fold, score in enumerate(grid_score.cv_validation_scores):
+            row = grid_score.parameters.copy()
+            row['fold'] = fold
+            row['score'] = score
+            rows.append(row)
+    df = pd.DataFrame(rows)
+    return df
 
-
-# # Plot Performance to Select Hyperparameters
 
 # In[19]:
 
-plot_ready = pd.DataFrame(cv_accuracy, columns=['type', 'AUROC', 'alpha', 'l1'])
+cv_score_df = grid_scores_to_df(clf_grid.grid_scores_)
+cv_score_df.head(2)
 
 
 # In[20]:
 
-plot_ready.head(5)
+# Cross-validated performance distribution
+facet_grid = sns.factorplot(x='l1_ratio', y='score', col='alpha',
+    data=cv_score_df, kind='violin', size=4, aspect=1)
+facet_grid.set_ylabels('AUROC');
 
 
 # In[21]:
 
-sns.factorplot(x="type", y="AUROC", hue="alpha",
-               col="l1", data=plot_ready, kind="box", size=4, aspect=0.6);
+# Cross-validated performance heatmap
+cv_score_mat = pd.pivot_table(cv_score_df, values='score', index='l1_ratio', columns='alpha')
+ax = sns.heatmap(cv_score_mat, annot=True, fmt='.1%')
+ax.set_xlabel('Regularization strength multiplier (alpha)')
+ax.set_ylabel('Elastic net mixing parameter (l1_ratio)');
 
 
-# # Use Optimal Hyperparameters to Output ROC Curve
+# ## Use Optimal Hyperparameters to Output ROC Curve
 
 # In[22]:
 
-# Select optimal hyperparameters based on performance in test-set
-optimal_alpha = 0.05
-optimal_l1 = 0.1
+y_pred_train = best_clf.decision_function(X_train)
+y_pred_test = best_clf.decision_function(X_test)
 
-# Initialize optimal classifier
-clf = SGDClassifier(loss=CLASSIFIER,
-                    penalty=PENALTY,
-                    alpha=optimal_alpha,
-                    l1_ratio=optimal_l1,
-                    class_weight='balanced',
-                    random_state=1234)
+def get_threshold_metrics(y_true, y_pred):
+    roc_columns = ['fpr', 'tpr', 'threshold']
+    roc_items = zip(roc_columns, roc_curve(y_true, y_pred))
+    roc_df = pd.DataFrame.from_items(roc_items)
+    auroc = roc_auc_score(y_true, y_pred)
+    return {'auroc': auroc, 'roc_df': roc_df}
 
-# Fit the classifier using the training data and predict the hold out set
-clf.fit(X_sub, y)
-y_score = clf.decision_function(X_sub)
-y_score_hold = clf.decision_function(X_holdout)
-
-# Get True/False Positive Rates
-fpr, tpr, _ = roc_curve(y, y_score)
-fpr_hold, tpr_hold, _ = roc_curve(Y_holdout, y_score_hold)
-
-# Get AUROC
-train_error = roc_auc_score(y, y_score, average='weighted')
-holdout_error = roc_auc_score(Y_holdout, y_score_hold, average='weighted')
+metrics_train = get_threshold_metrics(y_train, y_pred_train)
+metrics_test = get_threshold_metrics(y_test, y_pred_test)
 
 
 # In[23]:
 
 # Plot ROC
 plt.figure()
-plt.plot(fpr, tpr, label='Training ROC curve (weighted AUROC = {0:0.2f})'.format(train_error))
-plt.plot(fpr_hold, tpr_hold, label='Holdout ROC curve (weighted AUROC = {0:0.2f})'.format(holdout_error))
+for label, metrics in ('Training', metrics_train), ('Testing', metrics_test):
+    roc_df = metrics['roc_df']
+    plt.plot(roc_df.fpr, roc_df.tpr,
+        label='{} (AUROC = {:.1%})'.format(label, metrics['auroc']))
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-plt.title('Predicting TP53 Mutation in PanCancer Data')
-plt.legend(loc='lower right')
+plt.title('Predicting TP53 mutation from gene expression (ROC curves)')
+plt.legend(loc='lower right');
 
 
-# It looks like the holdout set actually performed BETTER by chance
-
-# # What is the classifier?
+# ## What are the classifier coefficients?
 
 # In[24]:
 
-classifier = pd.DataFrame(clf.coef_.T, index=X_matrix.columns, columns=['weight'])
-classifier['abs'] = classifier['weight'].abs()
-classifier = classifier.sort_values('abs', ascending=False)
+coef_df = pd.DataFrame(best_clf.coef_.transpose(), index=X.columns, columns=['weight'])
+coef_df['abs'] = coef_df['weight'].abs()
+coef_df = coef_df.sort_values('abs', ascending=False)
 
 
 # In[25]:
 
-classifier.head(10)
+'{:.1%} zero coefficients; {:,} negative and {:,} positive coefficients'.format(
+    (coef_df.weight == 0).mean(),
+    (coef_df.weight < 0).sum(),
+    (coef_df.weight > 0).sum()
+)
+
+
+# In[26]:
+
+coef_df.head(10)
 
 
 # The results are not surprising. TP53 is a transcription modulator and when it mutated in a tumor, the cell goes haywire. This makes finding a transcriptional signature fairly easy. Also, the genes that the classifier uses is interesting, but not necessarily novel.
@@ -250,3 +253,38 @@ classifier.head(10)
 # 2. SPATA18 is a gene associated with spermatogenesis and is a transcription factor for TP53. It's association with TP53 was [recently discovered](http://www.ncbi.nlm.nih.gov/pubmed/21300779) in 2011.
 # 3. C6orf138 (or [PTCHD4](http://www.genecards.org/cgi-bin/carddisp.pl?gene=PTCHD4)) is also a transcriptional target for TP53 and was only recently discovered in [2014 to repress hedgehog signalling](http://www.ncbi.nlm.nih.gov/pmc/articles/PMC4239647/).
 # 4. The list goes on and includes several other TP53 targets...
+
+# ## Investigate the predictions
+
+# In[27]:
+
+predict_df = pd.DataFrame.from_items([
+    ('sample_id', X.index),
+    ('testing', X.index.isin(X_test.index).astype(int)),
+    ('status', y),
+    ('decision_function', best_clf.decision_function(X)),
+    ('probability', best_clf.predict_proba(X)[:, 1]),
+])
+predict_df['probability_str'] = predict_df['probability'].apply('{:.1%}'.format)
+
+
+# In[28]:
+
+# Top predictions amongst negatives (potential hidden responders)
+predict_df.sort_values('decision_function', ascending=False).query("status == 0").head(10)
+
+
+# In[29]:
+
+# Ignore numpy warning caused by seaborn
+warnings.filterwarnings('ignore', 'using a non-integer number instead of an integer')
+
+ax = sns.distplot(predict_df.query("status == 0").decision_function, hist=False, label='Negatives')
+ax = sns.distplot(predict_df.query("status == 1").decision_function, hist=False, label='Positives')
+
+
+# In[30]:
+
+ax = sns.distplot(predict_df.query("status == 0").probability, hist=False, label='Negatives')
+ax = sns.distplot(predict_df.query("status == 1").probability, hist=False, label='Positives')
+
